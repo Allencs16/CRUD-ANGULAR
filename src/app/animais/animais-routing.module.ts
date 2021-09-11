6c3fb516097e20456e1b4cd27dc462eda0bde8be
Breno Alencar <brenoallencs@gmail.com>
2021-09-10 23:53:02 -0300

    Resolver

--- src/app/animais/animais-routing.module.ts
+++ src/app/animais/animais-routing.module.ts
@@ -1,12 +1,17 @@
-import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
+import { ListaAnimaisResolver } from './lista-animais/lista-animais.resolver';
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ListaAnimaisComponent,
+    resolve: {
+      animais: ListaAnimaisResolver
+    }
   },
   {
     path: ':animalId',
